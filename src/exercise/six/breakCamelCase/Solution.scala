a528263a54e4da1f185cb6c4ca95f4d7d05fcffb
данил метелев <dmetelev@embedika.ru>
2024-10-25 12:26:45 +0500

    InitialCommit Дальше будет хуже

---
+++ src/exercise/six/breakCamelCase/Solution.scala
@@ -0,0 +1,30 @@
+package exercise.six.breakCamelCase
+
+
+object Solution {
+    val wordsAndSolutions =
+        Map("helloWorld" -> "hello World",
+            "camelCase" -> "camel Case",
+            "breakCamelCase" -> "break Camel Case"
+        )
+
+    def main(args: Array[String]): Unit = {
+        val results = wordsAndSolutions.map { case (word, expected) =>
+            val ourSolution = SolutionGetrol.solute(word)
+            s"Solution is: ${ourSolution == expected}. Our solution was: \"$ourSolution\". Expected solution is: \"$expected\""
+        }
+        results.map(println)
+    }
+}
+
+
+object SolutionGetrol {
+    def solute(word: String): String = {
+        word.toUpperCase()
+    }
+
+}
+
+object SolutionAtlxdub {
+    def solute(word: String): String = ???
+}
