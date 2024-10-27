3dfb28753bd447e76a210e8bf406b3c530561857
getrol <getrol154@mail.ru>
2024-10-27 12:10:14 +0500

    AbstractSolution и CamelCase -Выделен общий родительский класс, который требует реализации. Тестовые данные и два вида решения. -Переписан метод проверки результатов. Теперь он также пишет чье решение, сколько успешно тестов было пройдено и затраченное время. -Решена задача на camelcase

--- src/exercise/eight/countOddBelowNumber/Solution.scala
+++ src/exercise/eight/countOddBelowNumber/Solution.scala
@@ -4,36 +4,25 @@ import general.AbstractSolution
 
 
 object Solution extends AbstractSolution[BigInt, BigInt] {
-  val inputAndOutput = Map(BigInt(2100000000) * BigInt(2100000000) * BigInt(2100000000) * BigInt(2100000000)
-    -> BigInt(1050000000) * BigInt(2100000000) * BigInt(2100000000) * BigInt(2100000000),
+  val inputAndOutput = Map(BigInt(210000000)
+    -> BigInt(105000000),
     BigInt(7) -> BigInt(3),
     BigInt(16) -> BigInt(8),
     BigInt(0) -> BigInt(0),
     BigInt(-4) -> BigInt(0))
 
+  override def getrolSolute(input: BigInt): BigInt = if (input > 0) input / BigInt(2) else 0
 
-  override val getrolSolution: Solution.PersonSolution = (input: BigInt) => if (input > 0) input / BigInt(2) else 0
-  override val atlxSolution: Solution.PersonSolution = new PersonSolution {
-    override def solute(input: BigInt): BigInt = {
-      def loop(count: BigInt, res: BigInt): BigInt = {
-        if (count > 0)
-          if (count % 2 == 0)
-            loop(count - 1, res)
-          else
-            loop(count - 1, res + 1)
-        else res - 1
-      }
-
-      loop(input, 0)
+  override def atlxSolute(input: BigInt): BigInt = {
+    def loop(count: BigInt, res: BigInt): BigInt = {
+      if (count > 0)
+        if (count % 2 == 0)
+          loop(count - 1, res)
+        else
+          loop(count - 1, res + 1)
+      else res - 1
     }
+
+    loop(input, 0)
   }
-}
-
-
-object SolutionGetrol {
-
-}
-
-object SolutionAtlxdub {
-
 }
--- src/general/AbstractSolution.scala
+++ src/general/AbstractSolution.scala
@@ -1,34 +1,42 @@
 package general
 
-abstract class AbstractSolution[T, R] {
+import java.time.Instant
+
+trait AbstractSolution[T, R] {
 
   val inputAndOutput: Map[T, R]
 
-  val getrolSolution: PersonSolution
-  val atlxSolution: PersonSolution
+  def getrolSolute(input: T): R
 
-  trait PersonSolution {
-    def solute(input: T): R
+  def atlxSolute(input: T): R
+
+  private def collectResult(name: String, solution: T => R): String = {
+
+    val headString = s"*********$name solutions*********\n"
+    val testResults = inputAndOutput.map { case (input, expected) =>
+      val startTime = System.currentTimeMillis()
+      val ourSolution = solution(input)
+      val finishTime = System.currentTimeMillis()
+      (ourSolution == expected, input, ourSolution, expected, finishTime - startTime)
+
+    }
+
+    val result = headString +
+      s"Successfully passed tests - ${testResults.count(_._1)}/${testResults.size}\n" +
+      testResults.zipWithIndex.map { case ((isSuccess, input, ourSolution, expected, time), index) =>
+        s""" Solution ${index + 1}: ${isSuccess.toString.toUpperCase()}
+           | Input value: "$input".
+           | Our solution was: \"$ourSolution\".
+           | Expected solution is: \"$expected\".
+           | Algorithm was in work $time milliseconds \n""".stripMargin
+      }.mkString("\n")
+    result
   }
 
-  def main(args: Array[String]): Unit = {
-    val getrolResults = "Getrol solutions: " +
-      inputAndOutput.zipWithIndex.map { case ((input, expected), index) =>
-        val ourSolution = getrolSolution.solute(input)
-        s"""Solution ${index + 1}: ${(ourSolution == expected).toString.toUpperCase()}.
-           |Input value: ${input}.
-           |Our solution was: \"$ourSolution\".
-           |Expected solution is: \"$expected\" \n""".stripMargin
-      }
 
-    val atlxResults = "Atlxdub solutions: " +
-      inputAndOutput.zipWithIndex.map { case ((input, expected), index) =>
-        val ourSolution = atlxSolution.solute(input)
-        s"""Solution ${index + 1}: ${(ourSolution == expected).toString.toUpperCase()}.
-           |Input value: ${input}.
-           |Our solution was: \"$ourSolution\".
-           |Expected solution is: \"$expected\" \n""".stripMargin
-      }
+  def main(args: Array[String]): Unit = {
+    val getrolResults = collectResult("Getrol", getrolSolute)
+    val atlxResults = collectResult("Atlxdub", atlxSolute)
 
     println(getrolResults)
     println(atlxResults)
