7088d1a83e5e8a0125959f11824351149ce47975
getrol <getrol154@mail.ru>
2024-11-05 19:00:32 +0500

    OrderPlease -Крутая задача, решено. Можно было бы оптимизировать

---
+++ src/exercise/six/orderPlease/Solution.scala
@@ -0,0 +1,22 @@
+package exercise.six.orderPlease
+
+import general.AbstractSolution
+
+
+object Solution extends AbstractSolution[String, String] {
+  override val inputAndOutput: Map[String, String] = Map(
+    "is2 Thi1s T4est 3a" -> "Thi1s is2 3a T4est",
+    "4of Fo1r pe6ople g3ood th5e the2" -> "Fo1r the2 g3ood 4of th5e pe6ople",
+    "" -> ""
+  )
+
+  override def getrolSolute(input: String): String = {
+    val splitWord = input.split(" ")
+    val indexToWordMap = splitWord.map(word => word.find(_.isDigit).map(_.toInt).getOrElse(0) -> word).sortBy(_._1)
+    indexToWordMap.map(_._2).mkString(" ")
+  }
+
+  override def atlxSolute(input: String): String = {
+    ""
+  }
+}
