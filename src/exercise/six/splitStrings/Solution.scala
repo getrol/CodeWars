8aeb051623e0cee211dd1a15f312fc7d87a28d19
Atlx <virtual.failik@gmail.com>
2025-03-28 10:42:36 +0500

    splitStrings task solution Atlx

--- src/exercise/six/splitStrings/Solution.scala
+++ src/exercise/six/splitStrings/Solution.scala
@@ -16,6 +16,18 @@ object Solution extends AbstractSolution[String, List[String]] {
   }
 
   override def atlxSolute(input: String): List[String] = {
-    List("")
+    if (input.length % 2 != 0) {
+      val newInput = input + "_"
+      splitStrAtlx(newInput)
+    }
+    else if (input.isEmpty) List("") else{
+      splitStrAtlx(input)
+    }
+  }
+
+  def splitStrAtlx(input: String, str: List[String] = List()): List[String] = {
+    if (input.isEmpty) str else {
+      splitStrAtlx(input.substring(2), str.appended(input.take(2)))
+    }
   }
 }
